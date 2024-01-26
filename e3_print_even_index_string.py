ef6ee503cb87017c5ab3e8a70196b6cc7ad296c1
juicyjuswa <134273315+juicyjuswa@users.noreply.github.com>
2024-01-27 00:09:41 +0800

    remove third exercise

--- e3_print_even_index_string.py
+++
@@ -1,7 +0,0 @@
-
-user_input = (input("Original String is "))
-user_input_1 = len(user_input)
-print("Printing only even index chars")
-
-for i in range(0, user_input_1, 2):
-    print(user_input[i])
