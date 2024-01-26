26f08a35139e359f2f4a618ff59e01f618061a22
juicyjuswa <134273315+juicyjuswa@users.noreply.github.com>
2024-01-27 00:09:18 +0800

    remove second exercise

--- e2_print_sum_current_previous_number.py
+++
@@ -1,9 +0,0 @@
-print("Printing current and previous number sum in a range(10)")
-start_num = 0
-for i in range(0, 10):
-    current_num = i
-    previous_num = i-1
-    if previous_num < 0:
-        previous_num = 0
-    sum = i + previous_num
-    print("Current Number ", current_num, "Previous Number", previous_num, "Sum: ", sum)
